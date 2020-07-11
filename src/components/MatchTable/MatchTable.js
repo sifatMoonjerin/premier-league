7ff06186ba1fcb1f6f57bdfdb83b64faaf6d7002
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-07-11 13:42:16 +0600

    loading...

--- src/components/MatchTable/MatchTable.js
+++ src/components/MatchTable/MatchTable.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TeamStat from '../TeamStat/TeamStat';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const MatchTable = () => {
     return (
         <div>
             <h1>Table</h1>
+            <CircularProgress />
             <TeamStat/>
         </div>
     );
